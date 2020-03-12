d7f42a2b87bee5c9dfafdd4eea4fd6612c07e8b4
unknown <ganwenjie1992@163.com>
2020-03-12 21:29:43 +0800

    modify svn_mount.sh

--- mount_dir/svn_mount.sh
+++ mount_dir/svn_mount.sh
@@ -2,3 +2,13 @@ sudo mkdir -p ospdemo
 
 mount -t cifs -o username=ganwenjie,password=gwj182516 //192.168.3.102/mount_dir ospdemo
 #sudo mount -o username=ganwenjie,password=gwj182516 //192.168.3.102/moubt_dir ospdemo
+
+
+sudo mkdir -p ospdemo
+
+mount -t cifs -o username=ganwenjie,password=gwj182516 //192.168.3.102/mount_dir ospdemo
+#sudo mount -o username=ganwenjie,password=gwj182516 //192.168.3.102/moubt_dir ospdemo
+sudo mkdir -p ospdemo
+
+mount -t cifs -o username=ganwenjie,password=gwj182516 //192.168.3.102/mount_dir ospdemo
+#sudo mount -o username=ganwenjie,password=gwj182516 //192.168.3.102/moubt_dir ospdemo
