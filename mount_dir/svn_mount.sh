177db339bafea42fd726ded6ac6b11440451299a
wejgan <59913041+wejgan@users.noreply.github.com>
2020-03-12 20:31:14 +0800

    Add files via upload

---
+++ mount_dir/svn_mount.sh
@@ -0,0 +1,4 @@
+sudo mkdir -p ospdemo
+
+mount -t cifs -o username=ganwenjie,password=gwj182516 //192.168.3.102/mount_dir ospdemo
+#sudo mount -o username=ganwenjie,password=gwj182516 //192.168.3.102/moubt_dir ospdemo
